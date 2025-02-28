f631c3a0f226dd92f0e2408c59d9d22a3caa8400
Leo Gertsenshteyn <146586+leoger@users.noreply.github.com>
2025-02-28 22:44:31 +0000

    Ignore v3 and fix pom version to make GH Action work

--- benchmark.sh
+++ benchmark.sh
@@ -4,6 +4,6 @@ mvn package
 
 mkdir reports
 
-java -jar nitrite-v3/target/benchmarks.jar -rf json -rff reports/nitrite-v3.json
+# java -jar nitrite-v3/target/benchmarks.jar -rf json -rff reports/nitrite-v3.json
 
 java -jar nitrite-v4/target/benchmarks.jar -rf json -rff reports/nitrite-v4.json
