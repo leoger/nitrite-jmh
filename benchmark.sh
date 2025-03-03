3513766ca03865790542353765f7df723bf0dcf7
Leo Gertsenshteyn <146586+leoger@users.noreply.github.com>
2025-03-02 23:19:11 -0800

    narrow down to a small, specific experiment

--- benchmark.sh
+++ benchmark.sh
@@ -8,4 +8,4 @@ mkdir reports
 
 #java -jar nitrite-v3/target/benchmarks.jar -rf json -rff reports/nitrite-v3.json
 
-java -jar nitrite-v4/target/benchmarks.jar -rf json -rff reports/nitrite-v4.json
+java -jar nitrite-v4/target/benchmarks.jar -rf json -rff reports/nitrite-v4-subset.json -prof stack
